795ec5b845d91405097b96a35d482840ced7375a
Smart Lazarus <smartlazarus@gmail.com>
2025-01-13 12:00:14 +0100

    delete products from cart when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/currency.js";
 
@@ -39,7 +39,9 @@ cart.forEach((cartItem) => {
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link js-delete-quantity-link link-primary" data-product-id="${
+                    matchingProduct.id
+                  }">
                     Delete
                   </span>
                 </div>
@@ -89,3 +91,14 @@ cart.forEach((cartItem) => {
 });
 
 document.querySelector(".js-order-summary").innerHTML = cartHTML;
+
+document.querySelectorAll(".js-delete-quantity-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    const productId = link.dataset.productId;
+
+    removeFromCart(productId);
+    console.log(cart);
+
+    document.querySelector(".js-order-summary").innerHTML = cartHTML;
+  });
+});
